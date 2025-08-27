ea5ad0d4d8808b73c333154a2cce91d3684dd6b5
Iain Backeberg <iainbackeberg@gmail.com>
2025-08-27 09:59:17 +0200

    Add files via upload

---
+++ js/analytics.js
@@ -0,0 +1,4 @@
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-WTDLZV4CGV"></script> 
+ //<script> 
+ //window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-WTDLZV4CGV'); 
+ //</script>
---
+++ js/ga_consent.js
@@ -0,0 +1,13 @@
+//<script>
+  //window.dataLayer = window.dataLayer || [];
+  //function gtag(){dataLayer.push(arguments);}
+
+  // Default deny cookies until user consents
+  //gtag('consent', 'default', {
+    //'analytics_storage': 'denied',
+    'ad_storage': 'denied'
+  //});
+
+  //gtag('js', new Date());
+  //gtag('config', 'G-WTDLZV4CGV', { anonymize_ip: true });
+//</script>
