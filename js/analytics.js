e668464fdccc0f421130e4eee86c4f0b068d6473
Iain Backeberg <iainbackeberg@gmail.com>
2025-09-12 08:27:35 +0200

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,4 +1,9 @@
-// <script async src="https://www.googletagmanager.com/gtag/js?id=G-WTDLZV4CGV"></script> 
- //<script> 
- //window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-WTDLZV4CGV'); 
- //</script>
+// Google tag (gtag.js)
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WTDLZV4CGV"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WTDLZV4CGV');
+</script>
