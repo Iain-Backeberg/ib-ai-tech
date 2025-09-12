b5e644c74c8000b3bf98b688f2d700400253177d
Iain Backeberg <iainbackeberg@gmail.com>
2025-09-12 08:28:45 +0200

    Update ga_consent.js

--- js/ga_consent.js
+++ js/ga_consent.js
@@ -1,13 +1,14 @@
-//<script>
-  //window.dataLayer = window.dataLayer || [];
-  //function gtag(){dataLayer.push(arguments);}
+// GA Consent Mode setup
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
 
   // Default deny cookies until user consents
-  //gtag('consent', 'default', {
-    //'analytics_storage': 'denied',
-    //'ad_storage': 'denied'
-  //});
+  gtag('consent', 'default', {
+    'analytics_storage': 'denied',
+    'ad_storage': 'denied'
+  });
 
-  //gtag('js', new Date());
-  //gtag('config', 'G-WTDLZV4CGV', { anonymize_ip: true });
-//</script>
+  gtag('js', new Date());
+  gtag('config', 'G-WTDLZV4CGV', { anonymize_ip: true });
+</script>
